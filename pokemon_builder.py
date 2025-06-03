cc0351c18acb8ada802c6bb707cc225eeac8b94f
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-06-03 19:05:15 -0400

    add type updates

--- pokemon_builder.py
+++ pokemon_builder.py
@@ -3,15 +3,22 @@ import requests
 base_url = "https://pokeapi.co/api/v2/"
 
 class Pokemon:
-    def __init__(self, name, types):
+    def __init__(self, name, types, abilities):
         self.name = name
         self.types = types
-        # self.ability = ability
+        self.abilities = abilities
+        self._ability = None
     
-    # @ability.setter
-    # def ability(self, new_ability):
-    #     pass
-
+    @property
+    def ability(self):
+        return self._ability
+    
+    @ability.setter
+    def ability(self, new_ability):
+        if new_ability in self.abilities:
+            self._ability = new_ability
+        else:
+            raise ValueError(f"Invalid Ability.")
 
 
 def get_pokemon_info(name):
@@ -32,11 +39,27 @@ if __name__ == "__main__":
     if pokemon_info:
         #since types can be one or more, we need to loop through the types and store them as a 
         types = [t["type"]["name"] for t in pokemon_info["types"]]
-        pokemon = Pokemon(pokemon_info["name"], types)
+        abilities = [a["ability"]["name"] for a in pokemon_info["abilities"]]
+
+        pokemon = Pokemon(pokemon_info["name"], types, abilities)
         #, pokemon_info["type"], pokemon_info["ability"]
         print(f"Name: {pokemon.name.capitalize()}")
         #need to loop AGAIN to capitalize all values :(
         print(f"Type(s): {', '.join([t.capitalize() for t in pokemon.types])}")
+        print(f"Possible Ability(s): {', '.join([a.capitalize() for a in pokemon.abilities])}")
+        try:
+            chosen_ability = input(f"Select an ability from: {', '.join(pokemon.abilities)}\n")
+            pokemon.ability = chosen_ability
+            print(f"{pokemon.name.capitalize()} now has the ability: {pokemon.ability.capitalize()}")
+        except ValueError as e:
+            print(e)
+        
+        print("*****************************************")
+        print(f"Your {pokemon.name.capitalize()}: \nType: {', '.join([t.capitalize() for t in pokemon.types])} \nAbility: {pokemon.ability.capitalize()}")
+        print("*****************************************")
+
+        # pick_ability()
+
 
         #only capitalized the first value
         # print(f"Type(s): {', '.join(pokemon.types).capitalize()}")
