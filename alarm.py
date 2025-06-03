63a510ab5c8aa0d9cd4490c44a1c82a072a6cb10
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-06-03 15:48:33 -0400

    Add error handling for input and audio file detection

--- alarm.py
+++ alarm.py
@@ -6,15 +6,35 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 #allows for the use of sound files
 import pygame
 
+def validate_time(alarm_time):
+    try:
+        datetime.datetime.strptime(alarm_time, "%H%M%S")
+        return True
+    except ValueError:
+        return False
+
+
 #defining our function
 def set_alarm(alarm_time):
+    if not validate_time(alarm_time):
+        print("Error: Invalid time format.")
+        return
     #initial print that grabs the input
     print(f"Alarm set for {alarm_time}")
+
     #set the location sound file in our library to a variable. Used a local path since its in the same folder
+    #also this was added to gitignore so you'll need your own version
     sound_file = "Clock_Ring.mp3"
+
+    #we need to make sure the sound file exists before trying to run it.
+    if not os.path.exists(sound_file):
+        print(f"Error: Sound file {sound_file} not found!")
+        return
+
     #we want to loop through and print each second until the alarm hits, so we will use a while loop
     #I went with a boolean version as I find them more readable
     is_running = True
+
     while is_running:
         #grabbing the current time from the datetime module. It is a method of the datetime class. I used the strtime method to convert the output to a string with the same format as our input.
         curr_time = datetime.datetime.now().strftime("%H:%M:%S")
@@ -24,17 +44,22 @@ def set_alarm(alarm_time):
         if curr_time == alarm_time:
             #print a wake up message
             print("Wake Up! 🛌")
+
             #calling the initialize method of the mixer class in pygame.
             pygame.mixer.init()
+
             #loading the sound file
             pygame.mixer.music.load(sound_file)
+
             #play the sound file
             pygame.mixer.music.play()
+
             #in order to make sure the sound file plays in full, I am using the get_busy() function in a while loop
             #otherwise the condition becoming false would end the loop prematurley
             while pygame.mixer.music.get_busy():
                 time.sleep(1)
             is_running = False
+
         #if not we sleep for 1s and check the comparitor again.
         time.sleep(1)
 
