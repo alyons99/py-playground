e501f849f21cbf34a494f29ebbdb350a4491ff25
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-06-04 16:09:54 -0400

    add new dig_clock file

---
+++ dig_clock.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTime, QTimer, Qt
+
+class Dig_Clock(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        pass
+
+if __name__ == "__main__":
+    main()
