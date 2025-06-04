925360d9da05fa94df37efea1e9a8a38c9cda0bc
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-06-04 16:16:55 -0400

    display qt UI

--- dig_clock.py
+++ dig_clock.py
@@ -5,10 +5,23 @@ from PyQt5.QtCore import QTime, QTimer, Qt
 class Dig_Clock(QWidget):
     def __init__(self):
         super().__init__()
+        self.time_label = QLabel("12:00:00",self)
+        self.timer = QTimer(self)
         self.initUI()
 
     def initUI(self):
-        pass
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(650,300,600,200)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+
+        self.time_label.setAlignment(Qt.AlignCenter)
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    clock = Dig_Clock()
+    clock.show()
+    #execute method
+    sys.exit(app.exec_())
