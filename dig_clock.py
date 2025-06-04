00283972dad8602e0f482f773492fea96df6fa54
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-06-04 16:47:31 -0400

    finish clock

--- dig_clock.py
+++ dig_clock.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import QTime, QTimer, Qt
+from PyQt5.QtGui import QFont, QFontDatabase
 
 class Dig_Clock(QWidget):
     def __init__(self):
@@ -18,12 +19,15 @@ class Dig_Clock(QWidget):
         self.setLayout(vbox)
 
         self.time_label.setAlignment(Qt.AlignCenter)
-        self.time_label.setStyleSheet("font-size: 100px;"
+        self.time_label.setStyleSheet("font-size: 150px;"
                                       "color: #39FF14;"
                                     )
                                       #"background-color: black;") redundant
         self.setStyleSheet("background-color: black;")
-
+        font_id = QFontDatabase.addApplicationFont("digital-7.ttf")
+        font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        my_font = QFont(font_family, 150)
+        self.time_label.setFont(my_font)
         self.timer.timeout.connect(self.updateTime)
         self.timer.start(1000)
         #self.updateTime()
